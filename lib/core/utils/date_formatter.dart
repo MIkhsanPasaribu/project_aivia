5caba7bc34ab7d263c4917c6809c01505881e819
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2025-11-06 01:21:45 +0700

    feat: Add relative time formatting to DateFormatter utility

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -85,4 +85,32 @@ class DateFormatter {
   static bool isFuture(DateTime dateTime) {
     return dateTime.isAfter(DateTime.now());
   }
+
+  /// Format relative time: "2 menit yang lalu", "1 jam yang lalu", etc
+  static String formatRelativeTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+
+    if (difference.inSeconds < 60) {
+      return 'beberapa detik yang lalu';
+    } else if (difference.inMinutes < 60) {
+      final minutes = difference.inMinutes;
+      return '$minutes menit yang lalu';
+    } else if (difference.inHours < 24) {
+      final hours = difference.inHours;
+      return '$hours jam yang lalu';
+    } else if (difference.inDays < 7) {
+      final days = difference.inDays;
+      return '$days hari yang lalu';
+    } else if (difference.inDays < 30) {
+      final weeks = (difference.inDays / 7).floor();
+      return '$weeks minggu yang lalu';
+    } else if (difference.inDays < 365) {
+      final months = (difference.inDays / 30).floor();
+      return '$months bulan yang lalu';
+    } else {
+      final years = (difference.inDays / 365).floor();
+      return '$years tahun yang lalu';
+    }
+  }
 }
