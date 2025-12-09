f52fce5457eff442407f2c93f937d5b08e011d3d
MIkhsanPasaribu <mikhsanpasaribu@gmail.com>
2025-12-10 00:38:25 +0700

    feat: Add Firebase Performance and Crashlytics plugins to settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -23,6 +23,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     // 🆕 Google Services Plugin for Firebase
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
 
 include(":app")
